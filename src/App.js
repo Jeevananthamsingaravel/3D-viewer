20d647fe73b327c567afe1921e14808a85cdf5ca
jeevanantham singaravel <jeevanantham.s@sudoboat.com>
2023-11-30 11:53:13 +0530

    usdz

--- src/App.js
+++ src/App.js
@@ -5,27 +5,27 @@ const App = () => {
    
 
     const modelFile = [
-        {
-            key: "renderGLB",
-            name: "GLB",
-            format: "GLB",
-            path: "https://ikea-q-001-delivery.sitecorecontenthub.cloud/api/public/content/5gNXQI3Wu0ei3y37cIZ6dQ_original?v=38ef80f8",
-            modalTitle: "GLB Viewer",
-        },
-        {
-            key: "renderDRACO",
-            name: "GLB_DRACO",
-            format: "DRACO",
-            path: "https://ikea-q-001-delivery.sitecorecontenthub.cloud/api/public/content/Phn4VV1osUaPMWR5viMgzQ_original?v=7e3eaec3",
-            modalTitle: "GLB_DRACO Viewer",
-        },
-        {
-            key: "renderGLTF",
-            name: "GLTF",
-            format: "GLTF",
-            modalTitle: "GLTF Viewer",
-            path:"https://ikea-q-001-delivery.sitecorecontenthub.cloud/api/public/content/16e5df20dde44bb185c97e0685d4a8b5?v=28b20e77",
-        },
+        // {
+        //     key: "renderGLB",
+        //     name: "GLB",
+        //     format: "GLB",
+        //     path: "https://ikea-q-001-delivery.sitecorecontenthub.cloud/api/public/content/5gNXQI3Wu0ei3y37cIZ6dQ_original?v=38ef80f8",
+        //     modalTitle: "GLB Viewer",
+        // },
+        // {
+        //     key: "renderDRACO",
+        //     name: "GLB_DRACO",
+        //     format: "DRACO",
+        //     path: "https://ikea-q-001-delivery.sitecorecontenthub.cloud/api/public/content/Phn4VV1osUaPMWR5viMgzQ_original?v=7e3eaec3",
+        //     modalTitle: "GLB_DRACO Viewer",
+        // },
+        // {
+        //     key: "renderGLTF",
+        //     name: "GLTF",
+        //     format: "GLTF",
+        //     modalTitle: "GLTF Viewer",
+        //     path:"https://ikea-q-001-delivery.sitecorecontenthub.cloud/api/public/content/16e5df20dde44bb185c97e0685d4a8b5?v=28b20e77",
+        // },
         {
             key: "renderUSDZ",
             name: "USDZ",
